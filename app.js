23d598d0b2eaf672e606a9bac41ebfcaa5e49f7d
Suyash Chopra <suyashchopra19@gmail.com>
2017-11-10 16:22:11 -0500

    basic setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+const socketio = require('socket.io');
+const path = require ('path');
+
+app.engine('html',nunjucks.render);
+app.set('view engine','html');
+var env = nunjucks.configure('views',{noCache:true});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'/pubic')));
+
+const server = app.listen(3000, function(){
+	console.log("Listening at 3000")
+});
